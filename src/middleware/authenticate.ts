00e702b63a609a0085bab547dcc025d18e9cda5d
Ihab E. McShea <ihabshea@gmail.com>
2021-04-08 20:44:32 +0200

    +Removal of nonsensical condition in the authorization middleware

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -7,21 +7,18 @@ const JWTR = jwtRedis.default;
 const redisClient = redis.createClient();
 const jwtr = new JWTR(redisClient);
 dotenv.config();
-const authenticateJWT = async(req: any, res: any, next: () => void): Promise<void> => {
+const authenticateJWT = async (req: any, res: any, next: () => void): Promise<void> => {
     const authHeader: string = req.headers?.authorization;
-    if (authHeader) {
-        const token: string = authHeader?.split(" ")[1];
-        const jwtSecret: any = process.env.JWT_SECRET;
-        try{
+
+    const token: string = authHeader?.split(" ")[1];
+    const jwtSecret: any = process.env.JWT_SECRET;
+    try {
         let user = await jwtr.verify(token, jwtSecret) || null;
-            req.user = user;
-            next();
-        }catch(err){
-            res.status(401).json({error:"Unauthorized access."});
-        }
-     
-    } else {
+        req.user = user;
         next();
+    } catch (err) {
+        res.status(401).json({ error: "Unauthorized access." });
     }
+
 }
 export default authenticateJWT;
